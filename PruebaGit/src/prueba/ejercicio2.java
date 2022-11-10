6cc38e32b7337b13567129b105e6cb9ec0718f66
usuario1daw <jchaman@g.educaand.es>
2022-11-10 21:51:03 +0100

    1

---
+++ PruebaGit/src/prueba/ejercicio2.java
@@ -0,0 +1,28 @@
+package prueba;
+
+public class ejercicio2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		short num1 = 8;
+		int mes = 12;
+		long distancia=300000;
+				
+		char letra = 'a';
+		float largo = 1e10f;
+		double precio= 35000.95;
+		
+		boolean ganasdeirme= true;
+		
+		System.out.println(num1);
+		System.out.println(mes);
+		System.out.println(distancia);
+		System.out.println(letra);
+		System.out.println(largo);
+		System.out.println(precio);
+		System.out.println(ganasdeirme);
+		
+	}
+
+}
