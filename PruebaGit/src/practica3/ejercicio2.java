450610b621324b8752a2ee214b74f6d882f6c334
jh0ny2k2 <117847861+jh0ny2k2@users.noreply.github.com>
2022-11-29 01:06:30 +0100

    Update ejercicio2.java

--- PruebaGit/src/practica3/ejercicio2.java
+++ PruebaGit/src/practica3/ejercicio2.java
@@ -1,9 +1,30 @@
 package practica3;
 
+import java.util.Scanner;
 public class ejercicio2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int numero = 0;
+		
+		System.out.println("Dime un numero");
+		try {
+			numero=Integer.parseInt(sc.nextLine());
+		}catch (Exception e){
+			System.out.println("Dime un numero entero");
+		}
+		
+		if (numero%2 == 0) {
+			System.out.println("Es divisible entre 2");
+		}else if (numero%3 == 0) {
+			System.out.println("Es divisible entre 3");
+			}else if (numero%5 == 0) {
+			System.out.println("Es divisible entre 5");
+					}else if (numero%10 == 0) {
+			System.out.println("Es divisible entre 10");
+						}
 
 	}
 
