4be1bfa81bf179fa63c350003efb6c79feb75b80
jh0ny2k2 <117847861+jh0ny2k2@users.noreply.github.com>
2022-11-29 01:21:39 +0100

    Update ejercicio1.java

--- PruebaGit/src/practica3/ejercicio1.java
+++ PruebaGit/src/practica3/ejercicio1.java
@@ -3,8 +3,19 @@ package practica3;
 public class ejercicio1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		int numero=1, numero1=5, numero2=6, mayor;
+		
+		mayor=0;
+		if ((numero>numero2) && (numero>numero1)) {
+			mayor=numero;
+		}else if ((numero<numero1) && (numero1>numero2)){
+			mayor=numero1;
+			}else mayor=numero2;
+		
+		System.out.println("El numero mayor es: " + mayor);
+		
+		
 	}
 
 }
