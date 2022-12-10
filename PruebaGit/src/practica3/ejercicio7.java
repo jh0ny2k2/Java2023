40e09d88019efa80ee82526ce00c3d329a29fe69
jh0ny2k2 <117847861+jh0ny2k2@users.noreply.github.com>
2022-12-10 16:31:29 +0100

    Update ejercicio7.java

--- PruebaGit/src/practica3/ejercicio7.java
+++ PruebaGit/src/practica3/ejercicio7.java
@@ -3,8 +3,16 @@ package practica3;
 public class ejercicio7 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		int i;
+		int contador = 0;
+		for (i=1;i<=100;i++){
+			if(i%2==1){
+				contador +=i;
+			}
+		}
+		System.out.println(contador);
+	
 	}
 
 }
