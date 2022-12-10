20bc3579d10d8aba64ab5c6233e2613236778461
jh0ny2k2 <117847861+jh0ny2k2@users.noreply.github.com>
2022-12-10 19:57:03 +0100

    Update ejercicio8.java

--- PruebaGit/src/practica3/ejercicio8.java
+++ PruebaGit/src/practica3/ejercicio8.java
@@ -1,10 +1,24 @@
 package practica3;
 
+import java.util.Scanner;
 public class ejercicio8 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Scanner sc = new Scanner (System.in);
+		
+		int numero, doble, triple;
+		
+		System.out.println("Dime un numero entero");
+		
+		numero=Integer.parseInt(sc.nextLine());
+		
+		doble=numero*2;
+		triple=numero*3;
+		
+		System.out.println("Numero integrado: " + numero);
+		System.out.println("Doble de " + numero + " -> " + doble);
+		System.out.println("Triple de " + numero + "  -> " + triple);
 	}
 
 }
