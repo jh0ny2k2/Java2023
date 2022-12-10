900635837c581c915beafcbbaa69b62ae9dfb668
jh0ny2k2 <117847861+jh0ny2k2@users.noreply.github.com>
2022-12-10 18:24:15 +0100

    Update ejercicio24.java

--- PruebaGit/src/practica3/ejercicio24.java
+++ PruebaGit/src/practica3/ejercicio24.java
@@ -1,10 +1,24 @@
 package practica3;
 
+import java.util.Scanner;
 public class ejercicio24 {
+		
+		public static void main( String[] args ) {
+		      int numero, invertido = 0, resto;
+		 
+		      Scanner sc = new Scanner( System.in );
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
-}
+		      System.out.println( "Introduce un número: " );
+		 
+		      numero = sc.nextInt();
+		 
+		      while( numero > 0 ) {
+		         resto = numero % 10;
+		         invertido = invertido * 10 + resto;
+		         numero /= 10;
+		      }
+		 
+		      System.out.println( "Número invertido: " + invertido );
+		   }
+		 
+		}
