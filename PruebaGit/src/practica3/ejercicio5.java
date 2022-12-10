4a3308888f2993ab70e5625a2571af5dc69a262b
jh0ny2k2 <117847861+jh0ny2k2@users.noreply.github.com>
2022-12-10 16:14:27 +0100

    Update ejercicio5.java

--- PruebaGit/src/practica3/ejercicio5.java
+++ PruebaGit/src/practica3/ejercicio5.java
@@ -1,10 +1,29 @@
 package practica3;
 
+import java.util.Scanner;
 public class ejercicio5 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Scanner sc= new Scanner(System.in);
+		int numero=0;
 
+		System.out.println("Dime un numero");
+		try {
+			numero=Integer.parseInt(sc.nextLine());
+		}catch (Exception e){
+			System.out.println("Un numero por favor");
+		}
+		
+		while ((numero>10) && (numero<1)){
+			System.out.println("Dime otro numero");
+			try {
+				numero=Integer.parseInt(sc.nextLine());
+			}catch (Exception e){
+				System.out.println("Un numero por favor");
+
+		}
 	}
-
 }
+}
+
