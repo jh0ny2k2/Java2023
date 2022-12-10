5570b1024373af988e23f817aecacd60d3ea22e7
jh0ny2k2 <117847861+jh0ny2k2@users.noreply.github.com>
2022-12-10 18:56:16 +0100

    Update ejercicio25.java

--- PruebaGit/src/practica3/ejercicio25.java
+++ PruebaGit/src/practica3/ejercicio25.java
@@ -1,10 +1,28 @@
 package practica3;
 
+import java.util.Scanner;
 public class ejercicio25 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	 public static void main(String[] args) {
+		 
+		 Scanner sc = new Scanner(System.in);
+	       int N, aux, inverso = 0, cifra;
+	        
+	           System.out.print("Introduce un número >= 10: ");
+	           N = sc.nextInt();
+	      
+	 
+	       aux = N;
+	       while (aux!=0){
+	           cifra = aux % 10;
+	           inverso = inverso * 10 + cifra;
+	           aux = aux / 10;
+	       }
+	
+	       if(N == inverso){
+	           System.out.println("Es capicua");
+	       }else{
+	           System.out.println("No es capicua");
+	       }
+	    }
 	}
-
-}
