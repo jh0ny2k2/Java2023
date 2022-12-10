7ad59cb5db27254e4b7fda4b6653b6cf00fd235a
jh0ny2k2 <117847861+jh0ny2k2@users.noreply.github.com>
2022-12-10 20:11:26 +0100

    Update ejercicio22.java

--- PruebaGit/src/practica3/ejercicio22.java
+++ PruebaGit/src/practica3/ejercicio22.java
@@ -1,10 +1,28 @@
 package practica3;
 
+import java.util.Scanner;
 public class ejercicio22 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner (System.in);
+		
+		int numero, i, numeromayor;
+		
+		System.out.println("Dime un numero de 2 o 3 cifras");
+		numero= Integer.parseInt(sc.nextLine());
+		
+		numeromayor=0;
+		for (i=1; i<=numero-1; i++) {
+			if (numero % i == 0) {
+				numeromayor = i;
+			}
+		}
+		
+		System.out.println("El divisor de " + numero + " es: " + numeromayor);
+
+		}
 
 	}
 
-}
+
