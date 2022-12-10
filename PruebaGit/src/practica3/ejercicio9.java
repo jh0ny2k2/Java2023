56b7df5c0d2c2d6866e46e85bdb2494393f27433
jh0ny2k2 <117847861+jh0ny2k2@users.noreply.github.com>
2022-12-10 17:59:10 +0100

    Update ejercicio9.java

--- PruebaGit/src/practica3/ejercicio9.java
+++ PruebaGit/src/practica3/ejercicio9.java
@@ -1,10 +1,29 @@
 package practica3;
 
+import java.util.Scanner;
 public class ejercicio9 {
+		
+		public static double factorial(double numero)  {
+			double acumulador=1;
+			
+			for(int i = (int) numero; i>=1; i--) {
+				acumulador = acumulador * i;
+			}
+			return acumulador;
+		}
+		
+		
+		public static void main(String[] args) {
+			double numero=0;
+			double acumulador=1;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+			Scanner sc = new Scanner(System.in);
+			
+			System.out.println("Dime un numero para calcular su factorial");
+			numero = Double.parseDouble(sc.nextLine());
+			
+			System.out.println("El factorial de " + numero + " es " + factorial(numero)) ;
+		}
 
-	}
 
 }
