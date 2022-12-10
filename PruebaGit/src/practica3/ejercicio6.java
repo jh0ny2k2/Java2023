2e26457c3025f7264e4b64578e44837294ac8802
jh0ny2k2 <117847861+jh0ny2k2@users.noreply.github.com>
2022-12-10 16:20:38 +0100

    Update ejercicio6.java

--- PruebaGit/src/practica3/ejercicio6.java
+++ PruebaGit/src/practica3/ejercicio6.java
@@ -1,10 +1,27 @@
 package practica3;
 
+import java.util.Scanner;
 public class ejercicio6 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Scanner sc= new Scanner(System.in);
+		int numero = 0;
+		int i;
+		
+		System.out.println("Dime un numero mayor de 2");
+		try {
+			numero=Integer.parseInt(sc.nextLine());
+		}catch (Exception e){
+			System.out.println("Un numero por favor");
+		}
+	
+		for (i=2 ; i<= numero ; i++) {
+			if (i%2==0) {
+				System.out.println(i);
+			}
+		}
+		
 	}
 
 }
