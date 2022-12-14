24648d3306644a9558a175143bf1614318c20518
jh0ny2k2 <117847861+jh0ny2k2@users.noreply.github.com>
2022-12-14 11:57:55 +0100

    Create ejercicio1array.java

---
+++ PruebaGit/src/practica4/ejercicio1array.java
@@ -0,0 +1,34 @@
+package Ejercicios;
+
+import java.util.Scanner;
+
+public class Ejercicio1 {
+	
+	public static void pintarVectores(int []vector) {
+		for (int i=0; i<vector.length; i++) {
+			System.out.println(vector[i]);
+		}
+		
+	}
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int vector[]= new int[10];
+		System.out.println("Dime los numeros");
+		
+		for (int i=0; i<vector.length; i++) {
+			vector[i]= sc.nextInt();
+			
+		}
+		pintarVectores(vector);
+		
+		System.out.println("En orden inverso: ");
+		for (int i=9; i>=0; i--) {
+			System.out.println(vector[i]);
+		}
+
+	}
+
+}
