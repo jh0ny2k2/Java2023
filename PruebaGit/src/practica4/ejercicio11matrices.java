450b61f96a7e7ad96a024c404498135820c04851
jh0ny2k2 <117847861+jh0ny2k2@users.noreply.github.com>
2022-12-14 12:10:11 +0100

    Create ejercicio11matrices.java

---
+++ PruebaGit/src/practica4/ejercicio11matrices.java
@@ -0,0 +1,30 @@
+package practica4;
+
+public class ejercicio11matrices {
+
+	public static void vectores ( int numero[][]) {
+		System.out.println("[");
+		for (int i=0; i<numero.length; i++) {
+			for (int j=0; j<numero.length; j++) {
+				System.out.print(numero[i][j] + ", ");
+			}
+		}
+	}
+	
+	public static void main(String[] args) {
+		
+		int [][] vector = new int [5][5];
+		
+		for (int i=0; i<5 ; i++) {
+			if(i==vector[i][0]) {
+				System.out.println();
+			}
+			for (int j=0; j<5; j++) {
+				vector[0][j] = 1;
+				System.out.println(vector[i][j]);
+			}
+		}
+
+	}
+
+}
