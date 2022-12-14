53459b15a65d07fac41f046997886b326b00e39c
jh0ny2k2 <117847861+jh0ny2k2@users.noreply.github.com>
2022-12-14 14:55:44 +0100

    Create ejercicio13matrices.java

---
+++ PruebaGit/src/practica4/ejercicio13matrices.java
@@ -0,0 +1,18 @@
+package practica4;
+
+import java.util.Scanner;
+public class Ejercicio13matrices {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		int [][]matriz = new int [3][4];
+		
+		System.out.println("Dime los numeros");
+		for (int i=0; i<matriz.length; i++) {
+				
+		}
+		
+	}
+
+}
